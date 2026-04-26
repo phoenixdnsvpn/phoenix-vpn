d9e0ed275737166b6713c01de904e1060b747c29
phoenixdnsvpn <noreply@vaydnsandroid.com>
2026-04-26 15:01:53 +0800

    Integrate modified vaydns source into main repo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.net2share.vaydns"
         minSdk = 24
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.7.1"
+        versionCode = 18
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
