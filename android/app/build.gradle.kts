0bbd536d771f9a695d8e4facb59360f8f9276f1c
phoenixdnsvpn <phoenixvpn@vaydns.net>
2026-09-05 20:46:44 +0800

    major update in VayDNS proxy and authentication

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "net.vaydns.phoenix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 57
-        versionName = "2.8.1"
+        versionCode = 58
+        versionName = "2.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
